444217d82cd318ffd38d540dda065b3a65b1caa0
Teh Ming Hwang <tehminghwang2023@gmail.com>
2024-02-21 17:27:18 +0000

    new

--- server/index.js
+++ server/index.js
@@ -299,7 +299,351 @@ app.get("/api/posts/latestcreation", async (req, res) => {
     }
 });
 
-/* This group of functions is for posts table only */
+/* This group of functions is for joining tables
+Table Schema: 
+posts (postid, userid, create_timestamp, last_update_timestamp, categoryid, header, 
+description, visibility, active)
+category (categoryid, category)
+likes (postid, like_userid)
+users (userid, firstname, lastname, universityid)
+universities (universityid, university)
+
+Output: 
+postid, userid, create_timestamp, last_update_timestamp, categoryid, header, 
+description, visibility, active, number_of_likes (for each postid, count the number of unique like_userid), universityid,
+university (using posts' userid to find users' userid to match the universityid with the universities table to find university),
+category (using posts'categoryid to find category in category table by matching categoryid),
+firstname (matched using userid), lastname (matched using userid)
+*/
+
+// Get all the posts ...../api/posts
+const getEnhancedPosts = async () => {
+    try {
+        const query = `
+            SELECT 
+                p.postid, 
+                p.userid, 
+                p.create_timestamp, 
+                p.last_update_timestamp, 
+                p.categoryid, 
+                p.header, 
+                p.description, 
+                p.visibility, 
+                p.active, 
+                COUNT(DISTINCT l.like_userid) AS number_of_likes, 
+                u.universityid, 
+                un.university, 
+                c.category
+            FROM 
+                posts p
+            LEFT JOIN 
+                likes l ON p.postid = l.postid
+            JOIN 
+                users u ON p.userid = u.userid
+            JOIN 
+                universities un ON u.universityid = un.universityid
+            JOIN 
+                category c ON p.categoryid = c.categoryid
+            GROUP BY 
+                p.postid, u.universityid, un.university, c.category
+            ORDER BY 
+                p.postid DESC;`;
+
+        const [results] = await pool.query(query);
+        return results;
+    } catch (error) {
+        console.error("SQL Error: ", error.message);
+        throw error;
+    }
+};
+
+app.get("/api/enhancedposts", async (req, res) => {
+    try {
+        const posts = await getEnhancedPosts();
+        res.json({ posts });
+    } catch (error) {
+        console.error("Error fetching enhanced posts:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+// dynamic get posts based on specified query fields
+// http://127.0.0.1:3001/api/enhanced-related-posts?userid=0001&active=1&createTimestamp=2024-02-19T02:00:00
+const getEnhancedRelatedPosts = async (queryParameters) => {
+    let baseQuery = `
+        SELECT 
+            p.postid, p.userid, p.create_timestamp, p.last_update_timestamp, p.categoryid, 
+            p.header, p.description, p.visibility, p.active, 
+            COUNT(DISTINCT l.like_userid) AS number_of_likes, 
+            u.firstname, u.lastname, u.universityid, un.university, c.category
+        FROM 
+            posts p
+        LEFT JOIN 
+            likes l ON p.postid = l.postid
+        INNER JOIN 
+            users u ON p.userid = u.userid
+        INNER JOIN 
+            universities un ON u.universityid = un.universityid
+        INNER JOIN 
+            category c ON p.categoryid = c.categoryid
+    `;
+    const queryParams = [];
+
+    baseQuery += " WHERE 1=1";
+
+    // Dynamically add conditions based on query parameters
+    if (queryParameters.postid) {
+        baseQuery += " AND p.postid = ?";
+        queryParams.push(queryParameters.postid);
+    }
+
+    if (queryParameters.userid) {
+        baseQuery += " AND p.userid = ?";
+        queryParams.push(queryParameters.userid);
+    }
+
+    if (queryParameters.categoryid) {
+        baseQuery += " AND p.categoryid = ?";
+        queryParams.push(queryParameters.categoryid);
+    }
+
+    if ('visibility' in queryParameters) {
+        baseQuery += " AND p.visibility = ?";
+        queryParams.push(queryParameters.visibility);
+    }
+
+    if ('active' in queryParameters) {
+        baseQuery += " AND p.active = ?";
+        queryParams.push(queryParameters.active);
+    }
+
+    if (queryParameters.createTimestamp) {
+        baseQuery += " AND p.create_timestamp >= ?";
+        queryParams.push(queryParameters.createTimestamp);
+    }
+
+    if (queryParameters.updateTimestamp) {
+        baseQuery += " AND p.last_update_timestamp >= ?";
+        queryParams.push(queryParameters.updateTimestamp);
+    }
+
+    baseQuery += " GROUP BY p.postid, u.firstname, u.lastname, u.universityid, un.university, c.category";
+
+    try {
+        const [results] = await pool.query(baseQuery, queryParams);
+        return results;
+    } catch (error) {
+        console.error("SQL Error: ", error.message);
+        throw error;
+    }
+};
+
+app.get("/api/enhanced-related-posts", async (req, res) => {
+    try {
+        const relatedPosts = await getEnhancedRelatedPosts(req.query);
+        res.json({ relatedPosts });
+    } catch (error) {
+        console.error("Error fetching related posts:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+// Get limited number of posts, dynamic filter sorting desc/asce for postid, create_timestamp, last_update_timestamp 
+const getEnhancedxPosts = async (queryParameters) => {
+    let baseQuery = `
+        SELECT 
+            p.postid, p.userid, p.create_timestamp, p.last_update_timestamp, p.categoryid, 
+            p.header, p.description, p.visibility, p.active, 
+            COUNT(DISTINCT l.like_userid) AS number_of_likes, 
+            u.firstname, u.lastname, u.universityid, un.university, c.category
+        FROM 
+            posts p
+        LEFT JOIN 
+            likes l ON p.postid = l.postid
+        INNER JOIN 
+            users u ON p.userid = u.userid
+        INNER JOIN 
+            universities un ON u.universityid = un.universityid
+        INNER JOIN 
+            category c ON p.categoryid = c.categoryid
+    `;
+    const queryParams = [];
+
+    // Existing code for dynamic filtering
+	if (queryParameters.postid) {
+		baseQuery += " AND p.postid = ?";
+		queryParams.push(queryParameters.postid);
+	}
+
+	if (queryParameters.userid) {
+		baseQuery += " AND p.userid = ?";
+		queryParams.push(queryParameters.userid);
+	}
+
+	if (queryParameters.categoryid) {
+		baseQuery += " AND p.categoryid = ?";
+		queryParams.push(queryParameters.categoryid);
+	}
+
+	if ('visibility' in queryParameters) { // Explicitly checking for presence to allow filtering by visibility=0
+		baseQuery += " AND p.visibility = ?";
+		queryParams.push(queryParameters.visibility);
+	}
+
+	if ('active' in queryParameters) { // Explicitly checking for presence to allow filtering by active=0
+		baseQuery += " AND p.active = ?";
+		queryParams.push(queryParameters.active);
+	}
+
+	if (queryParameters.createTimestamp) {
+		baseQuery += " AND p.create_timestamp >= ?";
+		queryParams.push(queryParameters.createTimestamp);
+	}
+
+	if (queryParameters.updateTimestamp) {
+		baseQuery += " AND p.last_update_timestamp >= ?";
+		queryParams.push(queryParameters.updateTimestamp);
+	}
+
+    baseQuery += " GROUP BY p.postid, u.firstname, u.lastname, u.universityid, un.university, c.category";
+
+    // Determine the sorting field and order
+    const validSortFields = ['p.postid', 'p.create_timestamp', 'p.last_update_timestamp'];
+    const sortField = validSortFields.includes(`p.${queryParameters.sortField}`) ? `p.${queryParameters.sortField}` : 'p.postid';
+    // Invert the sort order for the initial selection to get the last 'n' rows
+    const sortOrder = queryParameters.sortOrder === 'asc' ? 'DESC' : 'ASC';
+
+    // Add sorting to the query
+    baseQuery += ` ORDER BY ${sortField} ${sortOrder}`;
+
+    // Set the limit for the number of posts to retrieve
+    const limit = queryParameters.num ? parseInt(queryParameters.num, 10) : 10; // Default to 10 if not specified
+    queryParams.push(limit);
+
+    baseQuery += " LIMIT ?";
+
+    try {
+        // Fetch the last 'n' posts in the inverted order
+        const [results] = await pool.query(baseQuery, queryParams);
+
+        // If the original sortOrder was 'asc', we need to reverse the results since they were fetched in 'desc' order
+        if (queryParameters.sortOrder === 'asc') {
+            results.reverse();
+        }
+
+        return results;
+    } catch (error) {
+        throw error;
+    }
+};
+
+app.get("/api/enhanced-xposts", async (req, res) => {
+    try {
+        const posts = await getEnhancedxPosts(req.query);
+        res.json({ posts });
+    } catch (error) {
+        console.error("Error fetching xposts:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+//Get the latest update posts
+const getEnhancedLatestUpdatePosts = async (numberOfPosts) => {
+    try {
+        const query = `
+            SELECT 
+                p.postid, p.userid, p.create_timestamp, p.last_update_timestamp, p.categoryid, 
+                p.header, p.description, p.visibility, p.active, 
+                COUNT(DISTINCT l.like_userid) AS number_of_likes, 
+                u.firstname, u.lastname, u.universityid, un.university, c.category
+            FROM 
+                posts p
+            LEFT JOIN 
+                likes l ON p.postid = l.postid
+            INNER JOIN 
+                users u ON p.userid = u.userid
+            INNER JOIN 
+                universities un ON u.universityid = un.universityid
+            INNER JOIN 
+                category c ON p.categoryid = c.categoryid
+            WHERE 
+                p.active = 1 AND p.visibility = 1
+            GROUP BY 
+                p.postid, u.firstname, u.lastname, u.universityid, un.university, c.category
+            ORDER BY 
+                p.last_update_timestamp DESC
+            LIMIT 
+                ?`;
+
+        const [results] = await pool.query(query, [numberOfPosts]);
+        return results;
+    } catch (error) {
+        console.error("SQL Error: ", error.message);
+        throw error;
+    }
+};
+
+app.get("/api/posts/enhancedlatestupdate", async (req, res) => {
+    // Number of posts to fetch, passed as a query parameter
+    const numberOfPosts = req.query.num ? parseInt(req.query.num, 10) : 10; // Default to 10 posts if not specified
+
+    try {
+        const posts = await getEnhancedLatestUpdatePosts(numberOfPosts);
+        res.json({ posts });
+    } catch (error) {
+        console.error("Error fetching latest update posts:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+//Get the latest creation posts
+const getEnhancedLatestCreationPosts = async (numberOfPosts) => {
+    try {
+        const query = `
+            SELECT 
+                p.postid, p.userid, p.create_timestamp, p.last_update_timestamp, p.categoryid, 
+                p.header, p.description, p.visibility, p.active, 
+                COUNT(DISTINCT l.like_userid) AS number_of_likes, 
+                u.firstname, u.lastname, u.universityid, un.university, c.category
+            FROM 
+                posts p
+            LEFT JOIN 
+                likes l ON p.postid = l.postid
+            INNER JOIN 
+                users u ON p.userid = u.userid
+            INNER JOIN 
+                universities un ON u.universityid = un.universityid
+            INNER JOIN 
+                category c ON p.categoryid = c.categoryid
+            WHERE 
+                p.active = 1 AND p.visibility = 1
+            GROUP BY 
+                p.postid, u.firstname, u.lastname, u.universityid, un.university, c.category
+            ORDER BY 
+                p.create_timestamp DESC
+            LIMIT 
+                ?`;
+
+        const [results] = await pool.query(query, [numberOfPosts]);
+        return results;
+    } catch (error) {
+        console.error("SQL Error: ", error.message);
+        throw error;
+    }
+};
+
+app.get("/api/posts/enhancedlatestcreation", async (req, res) => {
+    // Number of posts to fetch, passed as a query parameter
+    const numberOfPosts = req.query.num ? parseInt(req.query.num, 10) : 10; // Default to 10 posts if not specified
+
+    try {
+        const posts = await getEnhancedLatestCreationPosts(numberOfPosts);
+        res.json({ posts });
+    } catch (error) {
+        console.error("Error fetching latest created posts:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
 
 
 
